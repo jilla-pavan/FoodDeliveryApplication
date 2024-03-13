03b180491a4306999c15c4215da5cb6d9f1dc1e4
jilla pavan <jillapavan22001@gmail.com>
2024-03-14 01:01:33 +0530

    commit

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,23 +1,25 @@
 import React from "react";
 
 function CategoryList({ data }) {
-  const { name, finalPrice, price, description } = data.card.info;
+  const { name, finalPrice, price, description, defaultPrice } = data.card.info;
   console.log(data);
   return (
-    <div className="w-[900px] bg-slate-200 m-4 p-4 flex justify-between items-center rounded-lg">
-      <div className="text-left">
-        {name} : ₹ {finalPrice || price / 100} /-
-        <div className="text-sm my-2">{description}</div>
-      </div>
-      <div className="">
-        <img
-          className="w-[150px] h-[100px] rounded-lg shadow-lg"
-          src={
-            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
-            data?.card?.info?.imageId
-          }
-          alt="mealImage"
-        />
+    <div className="">
+      <div className="w-[900px] bg-slate-200 m-4 p-4 flex justify-between items-center rounded-lg">
+        <div className="text-left w-[700px] font-semibold">
+          {name} : ₹ {finalPrice / 100 || price / 100 || defaultPrice / 100} /-
+          <div className="text-xs my-2 font-normal">{description}</div>
+        </div>
+        <div className="">
+          <img
+            className="w-[150px] h-[100px] rounded-lg shadow-lg"
+            src={
+              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
+              data?.card?.info?.imageId
+            }
+            alt="mealImage"
+          />
+        </div>
       </div>
     </div>
   );
